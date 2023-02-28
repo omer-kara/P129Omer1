3f110ff050a264bbc38fb475619f9b9e3043e3aa
omer-kara <122117943+omer-kara@users.noreply.github.com>
2023-02-28 19:10:01 +0100

    Update Deneme.java

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -2,6 +2,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("B129");
+        //Merhaba dunya
     }
 }
 //Restore . --> Tamamen Working Space'i temizler
